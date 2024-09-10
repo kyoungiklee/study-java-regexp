d742e36a0be7c2bd88854edf95c5685074bbefe2
kyoungik.lee <kyoungik.lee@gmail.com>
2024-09-10 18:51:29 +0900

    regex short step part 4

--- src/test/java/org/opennuri/study/regexp/learning/regular/expression/RegExShortSteps04Test.java
+++ src/test/java/org/opennuri/study/regexp/learning/regular/expression/RegExShortSteps04Test.java
@@ -15,8 +15,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 class RegExShortSteps04Test {
 
     @Test
-            @DisplayName("Step 16: No capturing groups")
-            void no_capturing_group() {
+    @DisplayName("Step 16: No capturing groups")
+    void no_capturing_group() {
         // In the two examples in the previous step, we were capturing text that we don't really need.
         // In the "File Size" task, we captured the spaces before the first digit of the first digit of the file size.
         // and in the "CSV" task, we captured the commas between each token. We don't need to capture these characters,
@@ -42,8 +42,8 @@ class RegExShortSteps04Test {
 
         list = matcher.results().map(matchResult -> {
             String result = "";
-            if(matchResult.group(1) != null) result = matchResult.group(1);
-            if(matchResult.group(2) != null) result = matchResult.group(2);
+            if (matchResult.group(1) != null) result = matchResult.group(1);
+            if (matchResult.group(2) != null) result = matchResult.group(2);
             return result;
         }).toList();
         list.forEach(log::info);
@@ -79,6 +79,97 @@ class RegExShortSteps04Test {
         matcher = Pattern.compile("<\\/?(\\w+)>").matcher(source);
         list = matcher.results().map(matchResult -> matchResult.group(1)).toList();
         list.forEach(log::info);
+
     }
 
+    @Test
+    @DisplayName("Step 17: backreferences")
+    void backreference() {
+        // Although I warned you in the introduction that trying to create an HTML parser with regular expression usually leads to  heartache,
+        // the last example is a nice transition to another (sometimes) useful feature of most regular expression: backreference.
+        // Backlinks are like repeating groups where you can try to grab the same text twice.
+        // But they differ in one important aspect - they will only capture the same text, character by character.
+        // While the repeating group will allow us to capture something like this
+
+        String source = "heyabcdefg hey heyo heyellow heyyyyyyyyy";
+        Matcher matcher = Pattern.compile("(he(?:[a-z])+)").matcher(source);
+        List<String> list = matcher.results().map(matchResult -> matchResult.group(1)).toList();
+        list.forEach(log::info);
+
+        matcher = Pattern.compile("(he([a-z])(\\2+))").matcher(source);
+        list = matcher.results().map(matchResult -> matchResult.group(3)).toList();
+        list.forEach(log::info);
+        // Repeating capturing groups are useful when you want to re-match the same pattern, while backreferences are good when you want to match the same text.
+        // for example, we could use a backreference to try and find the appropriate opening and closing HTML tags:
+        source = "<span style=\"color: red\">hey</span>";
+        matcher = Pattern.compile("<(\\w+)[^>]*>[^<]+<\\/\\1>").matcher(source);
+        assertThat(matcher.find()).isEqualTo(true);
+        assertThat(matcher.group()).isEqualTo("<span style=\"color: red\">hey</span>");
+
+        // Please note that this is an extremely simplified example and strongly recommend that you do not attempt to write HTML parser based on regular expression
+        // This is a very complex syntax, and you will most get likely get sick.
+        // Named capturing groups are very similar to backreferences. so I'll cover them briefly here.
+        // The only difference between backreferences and a named capture group is that... a named capture group has a name.
+
+        matcher = Pattern.compile("<(?<tag>\\w+)[^>]*>[^<]+</\\k<tag>>").matcher(source);
+        list = matcher.results().map(MatchResult::group).toList();
+        list.forEach(log::info);
+
+
+    }
+
+    @Test
+    @DisplayName("Step 18: look forward (lookahead) and look back (lookbehind)")
+    void look_forward_and_look_back() {
+        // We will now delve into some of advanced features of regular expressions.
+        // Everything up to step 16 use quite often.
+        // But these last few steps are only for people who are very serious about using regex to match very complex expressions
+        // In other words, regular expression wizards. "Looking ahead" and "Looking back" may sound complicated, but they are actually not that complicated
+        // They allow you to do something similar to what we did with non-capturing groups earlier - check if any text exists before or right after the actual text we want to match.
+        // For example, suppose we only want to match the names of things that people love, but only if they are enthusiastic about it(only if they end their sentence with and exclamation point)
+        // We could do something like:
+
+        String source = "I like desk. I appreciate stapler. I love lamp!";
+        Matcher matcher = Pattern.compile("(\\w+)(?=!)").matcher(source);
+
+        assertThat(matcher.find()).isEqualTo(true);
+        assertThat(matcher.group()).isEqualTo("lamp");
+
+        // You can see how the above capturing group, which usually matches any of word in the passage, only matches the word "lamp"
+        // A positive "look ahead"(?=!) means that we can only match sequence that end in ! but we don't actually match the exclamation mark character itself.
+        // This is an important distinction because with non-capturing groups, we match a character but don't capture it.
+        // With lookaheads and lookbehinds, we use a character to build our regular expression, but then we don't even match it against itself
+        // They do what they sound like - positive lookahead and lookbehind allow the regex engin to continue matching only when the text contained in the lookahead/lookbehind actually matches
+        // Negative lookahead and lookbehind do the opposite - they allow the regular expression to match only when the text contained in the lookahead / lookbehind does not match.
+        // For example,  we want to match method names only in the method sequence chain, not the object they operate in. In this case, each method name must be preceded by symbol "."
+        // A regular expression using a simple lookback can help here:
+
+        source = "myArray.flatMap.aggregate.summarise.print";
+
+        // In the text above, we match any sequence of characters in the word (\w+), but only if they are preceded by a "."
+        matcher = Pattern.compile("(?<=\\.)(\\w+)").matcher(source);
+        List<String> list = matcher.results().map(MatchResult::group).toList();
+        list.forEach(log::info);
+        assertThat(list).containsExactly("flatMap", "aggregate", "summarise", "print");
+        // We could achieve something similar using non-capturing groups, but the result is messier
+        matcher = Pattern.compile("(?:\\.)(\\w+)").matcher(source);
+        list = matcher.results().map(MatchResult::group).toList();
+
+        list.forEach(log::info);
+        assertThat(list).containsExactly(".flatMap", ".aggregate", ".summarise", ".print");
+
+        // Even though it's shorter, it matches characters we don't want
+        // While ths example may seem trivial, lookahead and lookbehind can really help us clean up our regular expression
+        // It's not long before the finish line! The following 2 tasks will bring us one step closer to it
+        // The negative lookbehind allows the regex engine to keep trying to find a match only if the text contained within the negative lookbehind is not rendered
+        // until the rest of the text with which to match
+        // For example, we could use a regular expression to match only last names of women attending a conference.
+        // To do this, we'd like to make sure the person's last name doesn't come before Mr.
+        // Can you write a regular expression for this?(You can assume last names are at least four characters long.)
+
+        source = "Mr. Brown, Ms. Smith, Mrs. Jones, Miss. Daisy, Mr. Green";
+        matcher = Pattern.compile("(?<!Mr\\. )([A-Z]\\w{4})").matcher(source);
+        list = matcher.results().map(MatchResult::group).toList();
+        assertThat(list).containsExactly("Smith", "Jones", "Daisy");
+    }
 }
