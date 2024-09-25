8424b3559474a34609501b2d6d9d4f602336bdbc
kyoungik.lee <kyoungik.lee@gmail.com>
2024-09-25 18:29:34 +0900

    다양한 사용법

--- src/test/java/org/opennuri/study/regexp/learning/regular/expression/Chapter11Test.java
+++ src/test/java/org/opennuri/study/regexp/learning/regular/expression/Chapter11Test.java
@@ -72,4 +72,79 @@ class Chapter11Test {
         list.forEach(log::info);
 
     }
+
+    @Test
+    @DisplayName("영국 우편번호")
+    void 영국_우편번호() {
+        //영국 우편번호는 다섯자리, 여섯자리, 일곱자리이면 숫자와 문자로 구성된다.
+        //우편번호는 두 부분으로 나뉘는데, 외부 우편번호와 내부 우편번호이다.
+        //외부번호는 한자리나 두자리로 이루어진 알파벳 문자가 오고 이 뒤를 이러 한자리나 두자리 숫자가 온다.
+        //또는 한 자리나 두 자리 문자뒤에 한 자리 숫자와 한 자리 문자가 차례로 온다.
+        //내부 번호는 항상 한 자리 숫자 뒤에 두 자리 문자가 온다.
+        //이때 C,I,K,M,O,V는 제외하는데 이 문자들은 내부 번호에서 전혀 사용하지 않는다.
+        //내부 번호와 외부번호는 빈칸으로 구분한다.
+        String source = "171 Kyverdale Road, London N16 6PS\n" +
+                "33 Main Street, Portsmouth, P01 3AX\n" +
+                "18 High Street, London, NW11 8AB";
+
+        Matcher matcher = Pattern.compile("[A-Z]{1,2}\\d[A-Z\\d]? \\d[ABD-HJLNP-UW-Z]{2}").matcher(source);
+        List<String> list = matcher.results().map(MatchResult::group).toList();
+        assertThat(list).hasSize(3);
+        list.forEach(log::info);
+    }
+
+    @Test
+    @DisplayName("미국 사회보장번호")
+    void 미국_사회보장번호() {
+        //미국사회보장번호는 숫자로 구성되어 있다
+        //세묶음으로 나뉘는데 각 부분은 하이픈으로구분된다.
+        //첫 부분은 세자리로 구성되고, 두번째 부분은 두 자리 숫자, 마지막 부분은 네 자리 숫자로 이루어진다.
+        String source = "John Smith: 123-45-6789";
+        Matcher matcher = Pattern.compile("\\d{3}-\\d{2}-\\d{4}").matcher(source);
+        List<String> list = matcher.results().map(MatchResult::group).toList();
+        assertThat(list).hasSize(1);
+        list.forEach(log::info);
+    }
+
+    @Test
+    @DisplayName("IP 주소")
+    void IP_주소() {
+        //ip주소는 네바이트로 이루어진다.
+        //IP주소는 숫자 네 묶음을 주로 마침표(.)로 구분한다.
+        String source = "localhost is 127.0.0.1.";
+        Matcher matcher = Pattern.compile("((25[0-5]|2[0-4]\\d|1\\d{2}|\\d{1,2})\\.){3}(25[0-5]|2[0-4]\\d|1\\d{2}|\\d{1,2})").matcher(source);
+        List<String> list = matcher.results().map(MatchResult::group).toList();
+        assertThat(list).hasSize(1);
+        list.forEach(log::info);
+    }
+
+    @Test
+    @DisplayName("URL 주소")
+    void URL_주소() {
+        String source = "http://www.forta.com/blog\n" +
+                "https://www.forta.com/blog/index.cfm\n" +
+                "http://www.forta.com\n" +
+                "http://ben:password@www.forta.com/\n" +
+                "http://localhost/index.php?ab=1&c=2\n" +
+                "http://localhost:8500";
+
+        Matcher matcher = Pattern.compile("https?:\\/\\/(\\w*:\\w*@)?[\\w.]+([:\\d]+)?(\\/([\\w\\/\\.]*(\\?\\S+)?)?)?").matcher(source);
+        List<String> list = matcher.results().map(MatchResult::group).toList();
+        assertThat(list).hasSize(6);
+        list.forEach(log::info);
+
+    }
+
+    @Test
+    @DisplayName("이메일 주소")
+    void 이메일_주소() {
+        String source = "My name is Ben Forta, and my\n" +
+                "email address is kyoung@email.forta.com, kyoungik.lee+1@gmail.com, kyoung.ik.lee+1@email.forta.com";
+
+        Matcher matcher = Pattern.compile("(\\w+\\.)*[\\w+]+@(\\w+\\.)+[A-Za-z]+").matcher(source);
+        List<String> list = matcher.results().map(MatchResult::group).toList();
+        assertThat(list).hasSize(3);
+        list.forEach(log::info);
+
+    }
 }
